72a3de58fc985671b649117976c8fed5541fc944
h461415832 <461415832@qq.com>
2016-05-12 17:20:49 +0800

    小明第二次提交。新建了一个Student类。希望大家参考参考

---
+++ app/src/main/java/com/example/huaming_lin/day29_drawerlayout/Student.java
@@ -0,0 +1,17 @@
+package com.example.huaming_lin.day29_drawerlayout;
+
+/**
+ * Created by HuaMing_Lin on 2016/5/12.
+ */
+public class Student {
+
+    //afasdfasdfadsfdsa
+
+    private int age;
+    private String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
